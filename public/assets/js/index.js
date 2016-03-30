b133dd393963f4b3cc038e3809d57f648d11c7ae
sebbel <sebastian@pancia.de>
2016-03-30 22:36:30 +0200

    first mockup

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,2 +1,22 @@
-console.log("Hello World");
-console.log(hoodie);
+var create = document.querySelector('#createNew');
+var listPending = document.querySelector('#listPending');
+var account = document.querySelector('#account');
+var content = document.querySelector('#content');
+
+create.addEventListener('click', function(){
+	var input = document.createElement('input');
+	input.setAttribute('type', 'text');
+	input.setAttribute('id', 'datefield');
+	content.appendChild(input);
+	input.addEventListener('change', function(){
+		console.log(input.value);
+	})
+});
+
+listPending.addEventListener('click', function() {
+	console.log('listPending');
+});
+
+account.addEventListener('click', function() {
+	console.log('Account');
+});
